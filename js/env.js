6e682221dedcf2737e8948f1491f9bfe9b652352
Jossydee <116879502+jossyd11@users.noreply.github.com>
2024-05-22 09:54:14 +0100

    Update env.js

--- js/env.js
+++ js/env.js
@@ -1,5 +1,5 @@
 const contractAddress = ""; // Paste the address of the NFT collection.
 // uncomment the chain you are using:
-const chainId = "0x507"; // Moonbase
+const chainId = "0xCa387b5893Fc48c0d745dEc3cBE74cCc1b147D63"; // Moonbase
 // const chainId = "0x504"; // Moonbeam
 // const chainId = "0x250"; // Astar
